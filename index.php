ba2e42462fd68ef425d1830ed6df40490df5135b
Radan Chlapek <radan.chlapek@gmail.com>
2026-02-25 18:17:43 +0100

    styling

--- index.php
+++ index.php
@@ -3,23 +3,23 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
 
-<link rel="stylesheet" href="/assets/css/style.css">
+<link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 
 <header class="header">
     <div class="header__content">
-    <img class="header__logo" src="/assets/images/zverino-logo.svg" alt="Zverino logo">
+    <img class="header__logo" src="assets/images/zverino-logo.svg" alt="Zverino logo">
     <nav class="menu">
         <ul class="menu__list">
             <li class="menu__item">
-                <a href="#"><img class="menu__item-icon" src="/assets/images/pes-ikona.svg">Psi</a>
+                <a href="#"><img class="menu__item-icon" src="assets/images/pes-ikona.svg">Psi</a>
             </li>
             <li class="menu__item">
-               <a href="#"><img class="menu__item-icon" src="/assets/images/kocka-ikona.svg"> Kočky</a>
+               <a href="#"><img class="menu__item-icon" src="assets/images/kocka-ikona.svg"> Kočky</a>
             </li>
             <li class="menu__item">
-              <a href="#"><img class="menu__item-icon" src="/assets/images/kun-ikona.svg">  Koně</a>
+              <a href="#"><img class="menu__item-icon" src="assets/images/kun-ikona.svg">  Koně</a>
             </li>
         </ul>
        
@@ -38,7 +38,7 @@
     </div>
 </header>
 <div class="hphero">
-    <img class="hp-hero__image" src="/assets/images/zverino-hero.png">
+    <img class="hp-hero__image" src="assets/images/zverino-hero.png">
     <div class="hp-hero__overlay"></div>
     <div class="hp-hero__content">
 
@@ -73,7 +73,7 @@
   <div class="search-filters__item">
 
     <label class="search-filters__option">
-      <input class="search-filters__input" type="checkbox" name="type[]" value="dogs">
+      <input class="search-filters__input" type="checkbox" name="type[]" value="dogs" checked>
 
       <span class="search-filters__text">
       <svg class="search-filters__icon" width="4.2442mm" height="4.2333mm" version="1.1" viewBox="0 0 4.2442 4.2333" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><g transform="translate(-127.52 -123.56)"><g transform="matrix(.0066146 0 0 .0066146 126.47 128.85)" fill="#fff"><path d="m440-160q-17 0-28.5-11.5t-11.5-28.5v-240l-232-296q-15-20-4.5-42t36.5-22h560q26 0 36.5 22t-4.5 42l-232 296v240q0 17-11.5 28.5t-28.5 11.5zm40-308 198-252h-396z"/></g></g></svg>
@@ -107,8 +107,8 @@
 </div>
 <main class="main">
 <section class="section container">
-    <div class="section__subtitle">Aktuálně</div>
-    <h2 class="section__title">Nejnovější inzeráty 
+    <div class="section__subtitle section__subtitle--center">Aktuálně</div>
+    <h2 class="section__title section__title--center">Nejnovější inzeráty 
 
 <svg class="section__title-icon"width="6.4046mm" height="5.922mm" version="1.1" viewBox="0 0 6.4046 5.922" xmlns="http://www.w3.org/2000/svg">
  <defs>
@@ -139,7 +139,7 @@
 
     <div class="card">
     <div class="card__header">
-        <img class="card__image" src="/assets/images/retriever.jpg">
+        <img class="card__image" src="assets/images/retriever.jpg">
         <div class="card__overlay"></div>
         <div class="card__added">Přidáno před 8 min</div>
     </div>
@@ -151,7 +151,7 @@
             <div class="card__meta-item">
 <svg class="card__meta-icon" width="3.4396mm" height="4.2333mm" version="1.1" viewBox="0 0 3.4396 4.2333" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><g transform="translate(-127.87 -102.94)"><g transform="matrix(.0055122 0 0 .0055122 126.94 107.7)" fill="#fff"><path d="m531-501q21-21 21-51t-21-51-51-21-51 21-21 51 21 51 51 21 51-21zm-51 310q119-107 179.5-197t60.5-161q0-105-68.5-174t-171.5-69-171.5 69-68.5 174q0 71 60.5 161t179.5 197zm0 95q-157-131-234.5-243.5t-77.5-209.5q0-134 89-224.5t223-90.5q133 0 222.5 90.5t89.5 224.5q0 97-77 209t-235 244z"/></g></g></svg>
    <div class="card__meta-content">
-        <span>Praha</span><span>12km</span>
+        <span>Praha</span><span>12 km</span>
         </div>
     </div>
 
@@ -178,7 +178,7 @@
 
  <div class="card">
     <div class="card__header">
-        <img class="card__image" src="/assets/images/kocka1.jpg">
+        <img class="card__image" src="assets/images/kocka1.jpg">
         <div class="card__overlay"></div>
          <div class="card__added">Přidáno před 8 min</div>
     </div>
@@ -190,7 +190,7 @@
             <div class="card__meta-item">
 <svg class="card__meta-icon" width="3.4396mm" height="4.2333mm" version="1.1" viewBox="0 0 3.4396 4.2333" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><g transform="translate(-127.87 -102.94)"><g transform="matrix(.0055122 0 0 .0055122 126.94 107.7)" fill="#fff"><path d="m531-501q21-21 21-51t-21-51-51-21-51 21-21 51 21 51 51 21 51-21zm-51 310q119-107 179.5-197t60.5-161q0-105-68.5-174t-171.5-69-171.5 69-68.5 174q0 71 60.5 161t179.5 197zm0 95q-157-131-234.5-243.5t-77.5-209.5q0-134 89-224.5t223-90.5q133 0 222.5 90.5t89.5 224.5q0 97-77 209t-235 244z"/></g></g></svg>
    <div class="card__meta-content">
-        <span>Praha</span><span>12km</span>
+        <span>Praha</span><span>12 km</span>
         </div>
     </div>
 
@@ -218,7 +218,7 @@
 
  <div class="card">
     <div class="card__header">
-        <img class="card__image" src="/assets/images/kun1.jpg">
+        <img class="card__image" src="assets/images/kun1.jpg">
         <div class="card__overlay"></div>
          <div class="card__added">Přidáno před 8 min</div>
     </div>
@@ -230,7 +230,7 @@
             <div class="card__meta-item">
 <svg class="card__meta-icon" width="3.4396mm" height="4.2333mm" version="1.1" viewBox="0 0 3.4396 4.2333" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><g transform="translate(-127.87 -102.94)"><g transform="matrix(.0055122 0 0 .0055122 126.94 107.7)" fill="#fff"><path d="m531-501q21-21 21-51t-21-51-51-21-51 21-21 51 21 51 51 21 51-21zm-51 310q119-107 179.5-197t60.5-161q0-105-68.5-174t-171.5-69-171.5 69-68.5 174q0 71 60.5 161t179.5 197zm0 95q-157-131-234.5-243.5t-77.5-209.5q0-134 89-224.5t223-90.5q133 0 222.5 90.5t89.5 224.5q0 97-77 209t-235 244z"/></g></g></svg>
    <div class="card__meta-content">
-        <span>Praha</span><span>12km</span>
+        <span>Praha</span><span>12 km</span>
         </div>
     </div>
 
@@ -246,7 +246,7 @@
     </div>
 
     <div class="card__footer">
-        <div class="card__price">25 000 Kč</div>
+        <div class="card__price">10 000 Kč</div>
         <a class="card__user" href="#">OD
             <svg class="card__verified-icon" width="4.7625mm" height="4.4979mm" version="1.1" viewBox="0 0 4.7625 4.4979" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><g transform="translate(-146.82 -105.48)"><g transform="matrix(.0055122 0 0 .0055122 146.55 110.38)" fill="#fff"><path d="m347-72-75-124-141-32 13-144-96-108 96-108-13-144 141-32 75-124 133 57 133-57 75 124 141 32-13 144 96 108-96 108 13 144-141 32-75 124-133-57zm82-264 238-237-51-51-187 186-85-84-51 51z"/></g></g></svg>
 
@@ -258,7 +258,7 @@
 
  <div class="card">
     <div class="card__header">
-        <img class="card__image" src="/assets/images/pes1.jpg">
+        <img class="card__image" src="assets/images/pes1.jpg">
         <div class="card__overlay"></div>
          <div class="card__added">Přidáno před 2 týdny</div>
     </div>
@@ -270,7 +270,7 @@
             <div class="card__meta-item">
 <svg class="card__meta-icon" width="3.4396mm" height="4.2333mm" version="1.1" viewBox="0 0 3.4396 4.2333" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><g transform="translate(-127.87 -102.94)"><g transform="matrix(.0055122 0 0 .0055122 126.94 107.7)" fill="#fff"><path d="m531-501q21-21 21-51t-21-51-51-21-51 21-21 51 21 51 51 21 51-21zm-51 310q119-107 179.5-197t60.5-161q0-105-68.5-174t-171.5-69-171.5 69-68.5 174q0 71 60.5 161t179.5 197zm0 95q-157-131-234.5-243.5t-77.5-209.5q0-134 89-224.5t223-90.5q133 0 222.5 90.5t89.5 224.5q0 97-77 209t-235 244z"/></g></g></svg>
    <div class="card__meta-content">
-        <span>Praha</span><span>12km</span>
+        <span>Praha</span><span>12 km</span>
         </div>
     </div>
 
@@ -305,14 +305,168 @@
     
 
     </div>
+    <button class="btn-green btn-center" type="button" style="margin-top:24px;">Zobrazit všechny inzeráty</button>
     </section>
-    <section class="section container">
-    <div class="section__subtitle">Jak to funguje</div>
-    <h2 class="section__title">Proč Zverino?
+    <div class="separator"></div>
+    <section class="section section--columns container">
+        <div class="section__left">
+        <div class="section__subtitle">Jak to funguje</div>
+    <h2 class="section__title">Proč Zverino?</h2>
+
+    <p>Zverino je místo, kde najdete ověřené chovatele a zdravá zvířata bez zbytečného rizika. Každý chovatel prochází kontrolou identity, u inzerátů ověřujeme čip a základní zdravotní informace a zároveň dbáme na dodržování welfare pravidel. </p>
+        <p>Díky tomu můžete vybírat s větším klidem, rychleji porovnat nabídky a jednoduše kontaktovat chovatele – vše přehledně na jednom místě.</p>
+
+    <button class="btn-green" type="button" style="margin-top:24px;"><svg width="19" height="19" version="1.1" viewBox="0 0 5.0271 5.0271" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><g transform="translate(-123.22 -67.807)"><g transform="matrix(.0066146 0 0 .0066146 122.56 73.495)" fill="#fff"><path d="m450-290h60v-160h160v-60h-160v-160h-60v160h-160v60h160zm30.07 190q-78.84 0-148.21-29.92t-120.68-81.21-81.25-120.63q-29.93-69.34-29.93-148.17 0-78.84 29.92-148.21t81.21-120.68 120.63-81.25q69.34-29.93 148.17-29.93 78.84 0 148.21 29.92t120.68 81.21 81.25 120.63q29.93 69.34 29.93 148.17 0 78.84-29.92 148.21t-81.21 120.68-120.63 81.25q-69.34 29.93-148.17 29.93z"/></g></g></svg>
+
+    Přidat inzerát</button>
+        </div>
+        <div class="section__right">
+            <ul class="list-checkmark">
+                <li class="list-checkmark__item">
+                <h3 class="list-checkmark__title">Ověření identity</h3>
+                <p class="list-checkmark__desc">Každý uživatel je u nás vždy ověřen.</p>
+                </li>
+
+                                <li class="list-checkmark__item">
+                <h3 class="list-checkmark__title">Kontrola čipů</h3>
+                <p class="list-checkmark__desc">Každé zvíře musí mít povinný čip.</p>
+                </li>
+
+                                                <li class="list-checkmark__item">
+                <h3 class="list-checkmark__title">Zdravotní testy</h3>
+                <p class="list-checkmark__desc">Prověřujeme zdravotní stav každého zvířete</p>
+                </li>
+
+                                                                <li class="list-checkmark__item">
+                <h3 class="list-checkmark__title">Welfare pravidla</h3>
+                <p class="list-checkmark__desc">Dodržujeme přísné zásady chovu zvířat.</p>
+                </li>
+            </ul>
+
+                <img class="why-zverino" src="assets/images/proc-zverino.png">
+        </div>
+ 
             </section>
 </main>
+
+
+<footer class="footer">
+    <div class="footer__content container">
+         <div class="footer__column">
+    <img class="header__logo" width="147" height="40" src="assets/images/zverino-logo.svg" alt="Zverino logo">
+                <p>Databáze ověřených chovatelů a zdravých zvířat.</p>
+            </div>
+<div class="footer__columns">
+
+    <div class="footer__column">
+     <h3 class="footer__subtitle-title">Kategorie</h3>
+
+  
+      <ul id="footer-list-2" class="footer__list">
+        <li class="footer__list-item">
+          <a class="footer__link" href="#">Psy</a>
+        </li>
+        <li class="footer__list-item">
+          <a class="footer__link" href="#">Kočky</a>
+        </li>
+        <li class="footer__list-item">
+          <a class="footer__link" href="#">Koně</a>
+        </li>
+        <li class="footer__list-item">
+          <a class="footer__link" href="#">Všechny inzeráty</a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="footer__column">
+      <h3 class="footer__subtitle-title">Informace</h3>
+
+ 
+      <ul id="footer-list-3" class="footer__list">
+        <li class="footer__list-item">
+          <a class="footer__link" href="#">Jak to funguje</a>
+        </li>
+        <li class="footer__list-item">
+          <a class="footer__link" href="#">Ověření chovatelů</a>
+        </li>
+        <li class="footer__list-item">
+          <a class="footer__link" href="#">Nápověda</a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="footer__column">
+        <h3 class="footer__subtitle-title">Dokumenty</h3>
+  
+      <ul id="footer-list-4" class="footer__list">
+        <li class="footer__list-item">
+          <a class="footer__link" href="#">Obchodní podmínky</a>
+        </li>
+        <li class="footer__list-item">
+          <a class="footer__link" href="#">Zásady ochrany osobních ůdajů</a></a>
+        </li>
+        <li class="footer__list-item">
+          <a class="footer__link" href="#">Nastavení cookies</a>
+        </li>
+
+      </ul>
+    </div> 
+
+        <div class="footer__column">
+        <h3 class="footer__subtitle-title">Kontakt</h3>
+  
+      <ul id="footer-list-4" class="footer__list">
+        <li class="footer__list-item">
+          <a class="footer__link" href="#"><svg width="20" height="15.789" version="1.1" viewBox="0 0 5.2917 4.1776" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><g transform="translate(-112.38 -98.295)"><g transform="matrix(.0069627 0 0 .0069627 111.68 103.73)" fill="#fff"><path d="m172.31-180q-30.31 0-51.31-21t-21-51.31v-455.38q0-30.31 21-51.31t51.31-21h615.38q30.31 0 51.31 21t21 51.31v455.38q0 30.31-21 51.31t-51.31 21zm307.69-277.69 320-204.62-6.15-57.69-313.85 200-313.85-200-6.15 57.69z"/></g></g></svg>
+</a> info@zverino.cz
+        </li>
+        <li class="footer__list-item">
+          <a class="footer__link" href="#"><svg width="17" height="17" version="1.1" viewBox="0 0 4.4979 4.4979" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"><g transform="translate(-115.68 -89.167)"><g transform="matrix(.0066146 0 0 .0066146 114.76 94.591)" fill="#fff"><path d="m777.61-140q-113.07 0-227.19-52.58-114.11-52.58-209.88-148.34-95.38-95.77-147.96-209.7-52.58-113.92-52.58-226.99 0-18 12-30.2 12-12.19 30-12.19h130.46q15.15 0 26.73 9.89 11.58 9.88 14.73 24.42l22.93 117.69q2.38 16.38-1 28.15-3.39 11.77-12.16 19.77l-92.38 89.93q22.3 40.84 50.96 77.27 28.65 36.42 62.04 69.57 32.92 32.93 70 61.16t80.08 52.54l89.76-90.54q9.39-9.77 22.74-13.7 13.34-3.92 27.73-1.92l111.07 22.62q15.15 4 24.73 15.46t9.58 26v129.69q0 18-12.19 30-12.2 12-30.2 12z"/></g></g></svg>
+</a> +420 123 456 789
+        </li>
+
+
+      </ul>
+    </div> 
+  </div>
+
+  
+</div>
+
+  <div class="footer__copyright container">
+  <div class="footer__copyright__content">
+    
+© 2026 Zverino
+  </div>  
+   </div>  
+</footer>
+
 </body>
 
-<footer>
-    
-</footer>
+
+<script>
+ (() => {
+  const el = document.querySelector(".why-zverino");
+  if (!el) return;
+
+  const clamp = (v, min, max) => Math.max(min, Math.min(max, v));
+  const MAX_DEG = 4; // kolik stupňů max
+
+  const onScroll = () => {
+    const r = el.getBoundingClientRect();
+    const vh = window.innerHeight;
+
+    // -1..1 (0 = střed obrazovky)
+    const center = r.top + r.height / 2;
+    const n = clamp((center - vh / 2) / (vh / 2), -1, 1);
+
+    // rotace podle pozice
+    el.style.transform = `rotate(${n * MAX_DEG}deg)`;
+  };
+
+  // první výpočet + poslouchat scroll/resize
+  onScroll();
+  window.addEventListener("scroll", onScroll, { passive: true });
+  window.addEventListener("resize", onScroll);
+})();
+</script>
